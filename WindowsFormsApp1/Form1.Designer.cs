a28ccc60fe9dd3a47040d7fcd3685c024656fd38
Arpit Vaghela <arpit74057@gmail.com>
2025-02-16 10:58:54 +0530

    Inital commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbPorts = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.txtSendData = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtReceivedData = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbPorts
+            // 
+            this.cmbPorts.FormattingEnabled = true;
+            this.cmbPorts.Location = new System.Drawing.Point(27, 27);
+            this.cmbPorts.Name = "cmbPorts";
+            this.cmbPorts.Size = new System.Drawing.Size(121, 28);
+            this.cmbPorts.TabIndex = 0;
+            this.cmbPorts.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(154, 27);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(99, 34);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(259, 27);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(99, 34);
+            this.btnDisconnect.TabIndex = 2;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            // 
+            // txtSendData
+            // 
+            this.txtSendData.Location = new System.Drawing.Point(561, 83);
+            this.txtSendData.Name = "txtSendData";
+            this.txtSendData.Size = new System.Drawing.Size(100, 26);
+            this.txtSendData.TabIndex = 3;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(561, 27);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(99, 34);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // txtReceivedData
+            // 
+            this.txtReceivedData.Location = new System.Drawing.Point(27, 260);
+            this.txtReceivedData.Multiline = true;
+            this.txtReceivedData.Name = "txtReceivedData";
+            this.txtReceivedData.Size = new System.Drawing.Size(289, 166);
+            this.txtReceivedData.TabIndex = 5;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(665, 27);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(99, 34);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(708, 406);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(56, 20);
+            this.lblStatus.TabIndex = 7;
+            this.lblStatus.Text = "Status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtReceivedData);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSendData);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cmbPorts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbPorts;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.TextBox txtSendData;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtReceivedData;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadAvailablePorts();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // This runs when the form loads. You can leave it empty or add logic.
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Handle selection change (you can leave it empty for now)
+        }
+
+        // Step 1: Load Available COM Ports
+        private void LoadAvailablePorts()
+        {
+            cmbPorts.Items.Clear();
+            cmbPorts.Items.AddRange(SerialPort.GetPortNames());
+            if (cmbPorts.Items.Count > 0)
+                cmbPorts.SelectedIndex = 0;  // Select first port
+            else
+                MessageBox.Show("No COM ports found. Connect a device and restart.");
+        }
+
+        // Step 2: Connect to Selected COM Port
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (cmbPorts.SelectedItem == null)
+            {
+                MessageBox.Show("Select a COM port first.");
+                return;
+            }
+
+            try
+            {
+                serialPort = new SerialPort(cmbPorts.SelectedItem.ToString(), 1152000, Parity.None, 8, StopBits.One);
+                serialPort.DataReceived += SerialPort_DataReceived;
+                serialPort.Open();
+                lblStatus.Text = "Connected to " + cmbPorts.SelectedItem.ToString();
+                btnConnect.Enabled = false;
+                btnDisconnect.Enabled = true;
+                btnSend.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        // Step 3: Handle Incoming Data from Serial Device
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = serialPort.ReadLine(); // Read incoming data
+            Invoke(new Action(() => txtReceivedData.AppendText(data + Environment.NewLine))); // Update UI safely
+        }
+
+        // Step 4: Send Data to Serial Device
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.WriteLine(txtSendData.Text);
+                txtSendData.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Serial port is not open.");
+            }
+        }
+
+        // Step 5: Disconnect Serial Port
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.Close();
+                lblStatus.Text = "Disconnected";
+                btnConnect.Enabled = true;
+                btnDisconnect.Enabled = false;
+                btnSend.Enabled = false;
+            }
+        }
+
+        // Step 6: Clear Received Data
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtReceivedData.Clear();
+        }
+
+        // Step 7: Close Serial Port When Closing App
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+                serialPort.Close();
+        }
+    }
+}
